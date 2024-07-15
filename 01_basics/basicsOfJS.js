2af867c6438f18adbfcbf5e079afe9a63ed4a204
Aftab alam <143246414+student-apna@users.noreply.github.com>
2024-07-15 11:31:10 +0000

    commit config

---
+++ 01_basics/basicsOfJS.js
@@ -0,0 +1,18 @@
+console.log("Aftab Alam");     // for printing
+
+// varibles 
+
+const accountId = 144;    // not changable
+let accountEmail = "aftabgla@gamil.com";
+var accountpassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
+
+// prefered not to use var
+// because of issue in block 
+
+
+
